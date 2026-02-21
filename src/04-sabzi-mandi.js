d266f748a6e61dcec881b5a492e8a18c35bad8e3
Uday Patil <udayp9020@gmail.com>
2026-02-21 22:19:37 +0530

    sabzi-mandi question solved

--- src/04-sabzi-mandi.js
+++ src/04-sabzi-mandi.js
@@ -49,20 +49,57 @@
  */
 export function addToCart(cart, item) {
   // Your code here
+
+   if(!Array.isArray(cart) ){
+    return -1;
+  }
+  if(item === "" || typeof item !== "string"){
+    return cart.length;
+  }  
+  return cart.push(item);
 }
 
 export function addUrgentItem(cart, item) {
   // Your code here
-}
+
+  if (!Array.isArray(cart)) {
+  return [];
+  }
+
+  if(typeof item !== "string"|| item.trim() === ""){
+    return cart;
+  }
+
+  cart.unshift(item);
+  return cart;
+} 
 
 export function removeLastItem(cart) {
   // Your code here
+  if(!Array.isArray(cart) || cart.length === 0){
+    return undefined;
+  }
+  const newcart = cart.pop();
+  return newcart;
 }
 
 export function isInCart(cart, item) {
   // Your code here
+
+  if(!Array.isArray(cart)){
+    return false;
+  }
+  return cart.includes(item);
 }
 
 export function mergeCarts(cart1, cart2) {
   // Your code here
+  if(!Array.isArray(cart1)){
+     cart1 = []
+  }
+  else if(!Array.isArray(cart2)){
+     cart2 = []
+  }
+  const mergecart = cart1.concat(cart2);
+  return mergecart;
 }
